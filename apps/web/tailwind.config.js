6eb92e0bdd2dcba31f02a668a214d0f9c04f24cc
guy <devtyl3r@gmail.com>
2022-11-08 08:28:31 -0400

    fix

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -33,6 +33,9 @@ module.exports = {
             ...colors,
             ...tailwindColors,
          },
+         transitionProperty: {
+            height: 'height',
+         },
       },
    },
    variants: {
